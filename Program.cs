d1f14b93037bcd97ce2ee8df2df3df5ead398177
ICOnce <noaellegaard@gmail.com>
2024-09-23 11:41:02 +0200

    Added combat

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
-﻿Sword sword = new Sword("Name", 10, 20);
-Warrior warriorA = new Warrior("Ragnar", 200, sword);
-Warrior warriorB = new Warrior("Lagertha", 240, sword);
+﻿
+Warrior warriorA = new Warrior("Ragnar", 300, new Sword("Excalibur", 30, 50));
+Warrior warriorB = new Warrior("Lagertha", 300, new Sword("Collosal Slayer", 40, 60));
 
-Console.WriteLine("Just after creation:");
-Console.WriteLine(warriorA.GetInfo());
-Console.WriteLine(warriorB.GetInfo());
+Console.WriteLine($"Warrior A is called {warriorA.Name}, and has {warriorA.HitPoints}HP");
+Console.WriteLine($"Warrior B is called {warriorB.Name}, and has{warriorB.HitPoints}HP");
 Console.WriteLine();
 
-int damageFromA = warriorA.DealDamage();
-int damageFromB = warriorB.DealDamage();
-warriorA.ReceiveDamage(damageFromB);
-warriorB.ReceiveDamage(damageFromA);
+while (!(warriorA.Dead || warriorB.Dead))
+{
+    //Damage for combat round
+    int damA = warriorA.DealDamage();
+    int damB = warriorB.DealDamage();
 
-Console.WriteLine("After damage:");
-Console.WriteLine(warriorA.GetInfo());
-Console.WriteLine(warriorB.GetInfo());
-Console.WriteLine();
+    //Warriors take damage
+    warriorA.ReceiveDamage(damB);
+    warriorB.ReceiveDamage(damA);
+
+    //Output result of round
+    Console.WriteLine($"{warriorA.Name} hit {warriorB.Name} for {damA}, {warriorB.Name} now has {warriorB.HitPoints} health");
+    Console.WriteLine($"{warriorB.Name} hit {warriorA.Name} for {damB}, {warriorA.Name} now has {warriorA.HitPoints} health \n");
+
+
+}
+if (warriorA.Dead && !warriorB.Dead) Console.WriteLine(warriorB.Name + " is victorious!");
+else if (!warriorA.Dead && warriorB.Dead) Console.WriteLine(warriorA.Name + " is victorious!");
+else Console.WriteLine("It's a draw");
